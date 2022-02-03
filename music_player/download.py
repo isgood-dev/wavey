c5bc33f3502f036ea961f49db163dd2528fb4387
Luke <69216256+acatiadroid@users.noreply.github.com>
2022-02-03 22:15:22 +0000

    Update download.py

--- music_player/download.py
+++ music_player/download.py
@@ -1,140 +1,140 @@
-import threading
-import pafy
-import os
-import shutil
-from youtubesearchpython import VideosSearch
-from moviepy.editor import VideoFileClip
-from tkinter import *
-from tkinter import filedialog
-from .config import view
-from .assets import cascadia
+# import threading
+# import pafy
+# import os
+# import shutil
+# from youtubesearchpython import VideosSearch
+# from moviepy.editor import VideoFileClip
+# from tkinter import *
+# from tkinter import filedialog
+# from .config import view
 
-def file_convert(title):
-    mp4 = f'./Audio bin/{title}.mp4'
-    mp3 = f'./Audio bin/{title}.mp3'
-    clip = VideoFileClip(mp4)
-    audioclip = clip.audio
-    audioclip.write_audiofile(mp3)
-    audioclip.close()
-    clip.close()
-    os.remove(f'./Audio bin/{title}.mp4')
 
-def download(*, title=None, link=None):
-    if not link and title:
-        search = VideosSearch(title, limit=1)
-        search = search.result()
+# def file_convert(title):
+#     mp4 = f'./Audio bin/{title}.mp4'
+#     mp3 = f'./Audio bin/{title}.mp3'
+#     clip = VideoFileClip(mp4)
+#     audioclip = clip.audio
+#     audioclip.write_audiofile(mp3)
+#     audioclip.close()
+#     clip.close()
+#     os.remove(f'./Audio bin/{title}.mp4')
 
-        result = search["result"][0]
-        url = result["link"]
-        name = result["title"]
+# def download(*, title=None, link=None):
+#     if not link and title:
+#         search = VideosSearch(title, limit=1)
+#         search = search.result()
+
+#         result = search["result"][0]
+#         url = result["link"]
+#         name = result["title"]
         
-        print(f"Downloading: {name} ({url})")
+#         print(f"Downloading: {name} ({url})")
 
-        video = pafy.new(url)
-        video = video.getbest()
+#         video = pafy.new(url)
+#         video = video.getbest()
         
-        video.download(
-            filepath="./Audio bin/"
-        )
-        file_convert(name)
+#         video.download(
+#             filepath="./Audio bin/"
+#         )
+#         file_convert(name)
 
-        return
+#         return
 
-def file_opener():
-    file = filedialog.askopenfile(
-        initialdir=".",
-        title="Select an MP3 file",
-        filetypes=(
-            ("MP3 files", "*.mp3"),
-            ("all files", "*.*")
-        )
-    )
-    if not input:
-        return
+# def file_opener():
+#     file = filedialog.askopenfile(
+#         initialdir=".",
+#         title="Select an MP3 file",
+#         filetypes=(
+#             ("MP3 files", "*.mp3"),
+#             ("all files", "*.*")
+#         )
+#     )
+#     if not input:
+#         return
 
-    filename = file.name.split("/")
-    filename = filename[len(filename)-1]
-    shutil.copyfile(file.name, f"./Audio bin/{filename}")
-    # TODO: update song list
+#     filename = file.name.split("/")
+#     filename = filename[len(filename)-1]
+#     shutil.copyfile(file.name, f"./Audio bin/{filename}")
+#     # TODO: update song list
 
-def download_window():
-    back_colour = view("back_colour")
-    fore_colour = view("fore_colour")
+# def download_window():
+#     back_colour = view("back_colour")
+#     fore_colour = view("fore_colour")
 
-    window = Toplevel()
-    window.configure(bg=view("back_colour"))
-    window.geometry("300x300")
-    window.wm_title("Add music")
-    window.iconbitmap("Assets/downloadicon.ico")
-    window.resizable(False, False)
+#     window = Toplevel()
+#     window.configure(bg=view("back_colour"))
+#     window.geometry("300x300")
+#     window.wm_title("Add music")
+#     window.iconbitmap("music_player/Assets/downloadicon.ico")
+#     window.resizable(False, False)
     
-    Label(
-        window,
-        text="Add song by name:",
-        bg=back_colour,
-        fg="white"
-    ).pack()
+#     Label(
+#         window,
+#         text="Add song by name:",
+#         bg=back_colour,
+#         fg="white"
+#     ).pack()
 
-    add_name_entry = Entry(
-        window,
-        fg="white",
-        bg=fore_colour,
-        font=cascadia
-    )
-    add_name_entry.pack()
+#     add_name_entry = Entry(
+#         window,
+#         fg="white",
+#         bg=fore_colour,
+#         font=cascadia
+#     )
+#     add_name_entry.pack()
     
-    download_name = Button(
-        window,
-        fg="white",
-        bg=fore_colour,
-        font=cascadia,
-        text="Download",
-        borderwidth=0,
-        command=lambda: threading.Thread(
-            target=lambda: download(
-                title=add_name_entry.get()
-            )
-        ).start()
-    )
-    download_name.pack()
+#     download_name = Button(
+#         window,
+#         fg="white",
+#         bg=fore_colour,
+#         font=cascadia,
+#         text="Download",
+#         borderwidth=0,
+#         command=lambda: threading.Thread(
+#             target=lambda: download(
+#                 title=add_name_entry.get()
+#             )
+#         ).start()
+#     )
+#     download_name.pack()
 
-    Label(
-        window,
-        text="Add song by YT URL:",
-        fg="white",
-        bg=back_colour,
-        font=cascadia
-    ).pack()
+#     Label(
+#         window,
+#         text="Add song by YT URL:",
+#         fg="white",
+#         bg=back_colour,
+#         font=cascadia
+#     ).pack()
 
-    add_by_url_entry = Entry(
-        window,
-        fg="white",
-        bg=fore_colour,
-        font=cascadia
-    )
-    add_by_url_entry.pack()
+#     add_by_url_entry = Entry(
+#         window,
+#         fg="white",
+#         bg=fore_colour,
+#         font=cascadia
+#     )
+#     add_by_url_entry.pack()
 
-    download_url = Button(
-        window,
-        fg="white",
-        bg=fore_colour,
-        font=cascadia,
-        text="Download",
-        borderwidth=0,
-        command=lambda: threading.Thread(
-            target=lambda: download(
-                link=add_by_url_entry.get()
-            )
-        ).start()
-    )
-    download_url.pack()
+#     download_url = Button(
+#         window,
+#         fg="white",
+#         bg=fore_colour,
+#         font=cascadia,
+#         text="Download",
+#         borderwidth=0,
+#         command=lambda: threading.Thread(
+#             target=lambda: download(
+#                 link=add_by_url_entry.get()
+#             )
+#         ).start()
+#     )
+#     download_url.pack()
 
-    import_music = Button(
-        window,
-        fg="white",
-        bg=fore_colour,
-        font=cascadia,
-        text="Import Music from PC",
-        command=file_opener
-    )
-    import_music.pack(pady=20)
+#     import_music = Button(
+#         window,
+#         fg="white",
+#         bg=fore_colour,
+#         font=cascadia,
+#         text="Import Music from PC",
+#         command=file_opener
+#     )
+#     import_music.pack(pady=20)

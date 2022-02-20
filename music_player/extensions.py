d8149444b080dcd87f66ff7652b7136d32255525
Luke <69216256+acatiadroid@users.noreply.github.com>
2022-02-20 16:21:16 +0000

    Add HoverButton subclass

--- music_player/extensions.py
+++ music_player/extensions.py
@@ -40,6 +40,23 @@ class ScrollbarFrame(tk.Frame):
         self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
 
 
+class HoverButton(tk.Button):
+    """
+    A class to change the colour of buttons to the accent colour when hovered over.
+    """
+    def __init__(self, master, **kw):
+        tk.Button.__init__(self, master=master, **kw)
+        self.defaultForeground = self["foreground"]
+        self.bind("<Enter>", self.on_enter)
+        self.bind("<Leave>", self.on_leave)
+
+    def on_enter(self, e):
+        self['foreground'] = self["activeforeground"]
+
+    def on_leave(self, e):
+        self['foreground'] = self.defaultForeground
+
+
 class TkinterCustomButton(tkinter.Frame):
     """ tkinter custom button with border, rounded corners and hover effect
         Arguments:  master= where to place button

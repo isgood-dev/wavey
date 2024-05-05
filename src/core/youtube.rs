2d5999af4cf9af0dd9534016313ed14698500a75
acatia <cooldudequzons@gmail.com>
2024-05-06 00:16:45 +0100

    Use rusty_ytdl instead of rustube

--- src/core/youtube.rs
+++ src/core/youtube.rs
@@ -1,32 +1,31 @@
 use std::{collections::HashMap, path::Path};
 
-use rustube::{Id, Video};
+use rusty_ytdl::{Video, VideoOptions, VideoQuality, VideoSearchOptions};
 
 use crate::sql;
 
 pub async fn download_from_url(url: String) -> bool {
-    let id = Id::from_raw(&url).unwrap();
+    let video_options = VideoOptions {
+        quality: VideoQuality::HighestAudio,
+        filter: VideoSearchOptions::Audio,
+        ..Default::default()
+    };
 
-    let video = Video::from_id(id.into_owned()).await.unwrap();
-
-    let download_path = Path::new("./assets/audio");
-
-    let audio_info = video.best_audio().unwrap();
-    let video_details = &video.best_audio().unwrap().video_details;
-
-    let mut video_info = HashMap::new();
-
-    video_info.insert("format_type".to_string(), audio_info.format_type.map_or_else(|| "None".to_string(),|ft| format!("{:?}", ft)));
-    video_info.insert("video_id".to_string(), video_details.video_id.to_string());
+    let video = Video::new_with_options(url, video_options).unwrap();
     
+    let video_info = video.get_info().await.unwrap();
 
-    let _ = video
-        .best_audio()
-        .unwrap()
-        .download_to_dir(download_path)
-        .await;
+    let path_str = format!("./assets/audio/{}.mp3", video_info.video_details.video_id.as_str());
+    let path = Path::new(&path_str);
 
-    let _ = sql::add_music(video_info);
+    video.download(path).await.unwrap();
 
+    let mut to_store = HashMap::new();
+    to_store.insert("format_type".to_string(), "mp3".to_string());
+    to_store.insert("video_id".to_string(), video_info.video_details.video_id);
+    to_store.insert("display_name".to_string(), video_info.video_details.title);
+    
+    let _ = sql::add_music(to_store);
+    
     true
 }
